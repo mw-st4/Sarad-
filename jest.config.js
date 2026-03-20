686fae2c5ce20196586c17e37ce725acfd33db3b
mw-st4 <ultrastate2005@gmail.com>
2026-03-20 15:03:47 +0300

    Add Jest configuration for TypeScript, test patterns, and coverage thresholds

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
